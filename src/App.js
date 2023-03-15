f2f94e244f15362d659f5ca233482c84976aa329
Brian Pelaez <pelaez_brian@hotmail.com>
2023-03-14 23:57:32 -0300

    Responsive fix horizontal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './index.css';
-import Card from './components/Card';
+import "./index.css";
+import Card from "./components/Card";
 
 function App() {
-
   return (
     <div className="App">
-      <Card/>
+      <Card />
     </div>
   );
 }
